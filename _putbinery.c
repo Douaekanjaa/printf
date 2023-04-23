282bd1e6b5fa28205f7bacc9e37ac2c1ab00b7b1
mouadia <ouadia.mohammed@outlook.de>
2023-04-23 20:01:52 +0200

    update

---
+++ _putbinery.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _putbinery - converts decimal to binery.
+ * @counter: counter.
+ * @b: number to be converted.
+ * Return: Nothing (void).
+ */
+void	_putbinery(int b, int *counter)
+{
+	char array[2] = "01";
+
+	if (b > 0)
+	{
+		_putbinery(b / 2, counter);
+		write(1, &array[b % 2], 1);
+		counter++;
+	}
+}
