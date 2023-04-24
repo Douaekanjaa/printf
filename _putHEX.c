1d840a6e6d0e0b217616f499199e8b87e083fbea
mouadia <ouadia.mohammed@outlook.de>
2023-04-24 10:30:43 +0200

    X

---
+++ _putHEX.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _puthex - convertes from dec to hex in lowcase.
+ * @n: giving number.
+ * @counter: pointer to couter.
+ * Return: Nothing (void).
+*/
+
+void _puthex(unsigned int n, int *counter)
+{
+	char digits[17] = "0123456789ABCDEF";
+	char buf[9];
+	int i;
+
+	buf[8] = '\0';
+
+	for (i = 7; i >= 0; i--)
+	{
+		buf[i] = digits[n & 0xF];
+		n >>= 4;
+	}
+
+	for (i = 0; i < 8; i++)
+	{
+		write(1, &buf[i], 1);
+	}
+
+	counter++;
+}
