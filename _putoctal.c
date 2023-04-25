b9aab66e1249d13085cde3005f8f47d05c45640c
Dua <kanjaadouae1@gmail.com>
2023-04-25 18:08:47 +0000

    print octal

--- _putoctal.c
+++ _putoctal.c
@@ -1,6 +1,20 @@
 #include "main.h"
  
 /**
-  * _putoctal -
-  *
+  * _putoctal - Converts a decimal number to an octal number
+  * @n: the decimal number to be converted
+  * @counter: A pointer
+  * Return: nothing
 */
+
+void _putoct(unsigned int num, int *res)
+{
+	if (num > 0)
+	{
+		_putoct(num / 8, res);
+		_putchar((num % 8) + '0');
+
+		*res += 1;
+	}
+}
+
