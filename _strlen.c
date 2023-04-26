c34c795714fc824dbfa2641aa6e0a1dc6ac66514
Dua <kanjaadouae1@gmail.com>
2023-04-26 08:03:01 +0000

    _strlen func

---
+++ _strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * _strlen - length of a string
+  * @s: string
+  * Return: length
+*/
+
+int _strlen(const char *s)
+{
+	int lenght = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	return (length);
+}
+
