241df0354328b961d0d7399d8d2f563b1feb0839
Dua <kanjaadouae1@gmail.com>
2023-04-28 20:06:43 +0000

    update main.h

--- main.h
+++ main.h
@@ -10,13 +10,7 @@ int _printf(const char *format, ...);
 int checker(const char *format, va_list args, int *i);
 int _putnbr(int nb, int *counter);
 int _putstr(char *str);
-
-void _putoct(unsigned int num, int *res);
-
-void _puthex(unsigned int n, int *counter);
-void _putHEX(unsigned int n, int *counter);
-
-int    _putbinary(unsigned int number, int *counter);
+int _putbinary(unsigned int number, int *counter);
 
 int _strlen(const char *s);
 
