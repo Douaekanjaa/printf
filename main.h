47c88aa11d7d354c413038fd5f10bd4b93895a11
Dua <kanjaadouae1@gmail.com>
2023-04-28 10:13:29 +0000

    update main.h

--- main.h
+++ main.h
@@ -5,17 +5,24 @@
 #include <unistd.h>
 
 int _putchar(char c);
+
 int _printf(const char *format, ...);
 int checker(const char *format, va_list args, int *i);
+int checker2(const char *format, va_list args, int *i);
+
 int _putnbr(int nb, int *counter);
 int _putstr(char *str);
+
 void _putu(unsigned int n, int base, int *counter);
 void _putoct(unsigned int num, int *res);
+
 void _puthex(unsigned int n, int *counter);
-int handles_s(va_list args, int *res);
 void _putHEX(unsigned int n, int *counter);
+
 int    _putbinary(unsigned int number, int *counter);
+
 int _reversed(va_list args);
+
 int _strlen(const char *s);
 
 #endif
