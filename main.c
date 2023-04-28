584f55241669dd0cde058935ce4f1fbaa8d1ead6
Dua <kanjaadouae1@gmail.com>
2023-04-28 11:00:25 +0000

    update

--- main.c
+++ main.c
@@ -11,24 +11,6 @@
 
 int main(void)
 {
-	int len1;
-
-	char str1[] = "Hello, world!";
-	/* char str2[] = "";
-	char *str3 = NULL;*/
-
-
-	len1 = _reversed(str1);
-	_printf("\nLenght of str1: %d\n", len1);
-	
-	/*len2 = _reversed(str2);
-    	_printf("\nLength of str2: %d\n", len2);
-	printf("\nLength of str2: %d\n", len2);*/
-
-    	/* len3 = _reversed(str3);
-    	printf("\nLength of str3: %d\n", len3);
-	printf("\nLength of str3: %d\n", len3);*/
-
 
     return (0);
 }
